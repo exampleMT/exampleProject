fc4644111717dfa8f304c3401ee9becc7d1e4b4b
Mashrura.T <mashrura@ualberta.ca>
2017-09-25 11:58:47 -0600

    Adding fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int fibonacci(int num)
+{
+    if(num==0)
+        return 0;
+    else if(num==1)
+        return 1;
+    else
+        return fibonacci(num-1)+fibonacci(num-2);
+}
+
+int main()
+{
+    int n,result;
+    while(1)
+    {
+        printf("Enter a positive number: ");
+        scanf("%d",&n);
+
+
+        result=fibonacci(n);        //Calling recursive function
+        printf("Output = %d\n",result);
+    }
+
+
+    return 0;
+}
